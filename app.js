abd1edc0553e3db1c5c54032d7cd74f06575e1c3
hanthienduc <hanthienduc.96@gmail.com>
2022-08-06 12:52:53 +0900

    remove static file

--- app.js
+++ app.js
@@ -12,7 +12,6 @@ const cors = require("cors");
 app.use(cors());
 app.use(express.json());
 app.use(cors());
-app.use(express.static(path.join(__dirname, "client", "build")));
 app.use(express.urlencoded({ limit: '10mb', extended: false }))
 dbConnect()
 app.use('/', indexRouter)
